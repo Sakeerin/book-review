3b59b274fe71b703aa6e0fe42e2ab3b5b188d490
sakeerin <theblue0800@gmail.com>
2023-08-21 11:31:35 +0700

    cache and caching queries

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
@@ -27,7 +28,12 @@ class BookController extends Controller
              default => $books->latest(),
         };
         
-        $books = $books->get();
+        // $books = $books->get();
+
+        $cacheKey = 'books:' . $filter . ':' . $title;
+
+        // $books = Cache::remember('books', 3600, fn() => $books->get());
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', compact('books'));
     }
@@ -53,12 +59,20 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
+        $cacheKey = 'book:' . $book->id;
+
+        $book = cache()->remember($cacheKey, 3600, fn() => $book->load([
+            'reviews' => fn($query) => $query->latest()
+        ]));
+        
         return view(
             'books.show',
             [
-                'book' => $book->load([
-                    'reviews' => fn($query) => $query->latest()
-                ])
+                // 'book' => $book->load([
+                //     'reviews' => fn($query) => $query->latest()
+                // ])
+                // after use caching
+                'book' => $book
             ]
         );
     }
